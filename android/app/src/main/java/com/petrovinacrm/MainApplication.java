4d620881caf5cf3791bfee00564e0413e8b02ca3
Felipe Fontana <felipefontanaf@gmail.com>
2018-04-23 13:15:38 -0400

    change version to work release

--- android/app/src/main/java/com/petrovinacrm/MainApplication.java
+++ android/app/src/main/java/com/petrovinacrm/MainApplication.java
@@ -3,6 +3,7 @@ package com.petrovinacrm;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import in.sriraman.sharedpreferences.RNSharedPreferencesReactPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -27,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
         protected List<ReactPackage> getPackages() {
             return Arrays.<ReactPackage>asList(
                     new MainReactPackage(),
+            new RNSharedPreferencesReactPackage(),
                     new RealmReactPackage(),
                     new VectorIconsPackage(),
                     new SvgPackage(),
